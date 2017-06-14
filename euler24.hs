31783c1a49be9c05ce0bd0583befba7a68d29bf7
syzer3@gmail.com <syzer3@gmail.com>
2017-06-14 22:07:44 +0200

    was off by one

--- euler24.hs
+++ euler24.hs
@@ -2,5 +2,8 @@ import Data.List
 
 --012   021   102   120   201   210
 
+firstTry :: [Char]
+firstTry = sort $ permutations "012" !! 3
+
 main :: IO()
-main = print $ sort $ permutations "012" !! 3
+main = print $ (sort $ permutations "0123456789") !! (1000000-1)
